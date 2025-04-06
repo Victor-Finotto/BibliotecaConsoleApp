ab2d1e2e1dec17a125d390ae22c8c7a7b41b2036
Victor-Finotto <victor.finotto@outlook.com>
2025-04-06 16:47:57 -0300

    feat: implementa entidade Livro e métodos iniciais do LivroRepositorio (CRUD)

--- Entidades/Livro.cs
+++ Entidades/Livro.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BibliotecaConsoleApp.Interfaces;
 
 namespace BibliotecaConsoleApp.Entidades
 {
-    class Livro
+    class Livro : IEntidade
     {
+        public required int Id { get; init; }
+        public required string Titulo { get; set; }
+        public required string Autor { get; set; }
+        public required int AnoPublicacao { get; set; }
+        public required string Genero { get; set; }
     }
 }
--- Repositorios/LivroRepositorio.cs
+++ Repositorios/LivroRepositorio.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BibliotecaConsoleApp.Entidades;
+using BibliotecaConsoleApp.Interfaces;
 
 namespace BibliotecaConsoleApp.Repositorios
 {
-    class LivroRepositorio
+    class LivroRepositorio : IRepositorio<Livro>
     {
+        public List<Livro> Livros { get; set; } = [];
+        public void Adicionar(Livro entidade)
+        {
+            if (Livros.Contains(entidade))
+            {
+                throw new Exception("LIVRO JÁ EXISTENTE.");
+            }
+        
+            Livros.Add(entidade);
+        }
+
+        public void Atualizar(Livro entidade)
+        {
+            // TODO: Implementar lógica de atualização de dados do livro
+            // Essa lógica será feita no serviço e chamada aqui
+            throw new NotImplementedException();
+        }
+
+        public Livro? BuscarPorId(int id)
+        {
+            Livro? livroProcurado = Livros.Find(l => l.Id == id);
+
+            if (!(livroProcurado is null))
+            {
+                return livroProcurado;
+            }
+            else
+            {
+                throw new Exception("LIVRO NÃO ENCONTRADO.");
+            }
+        }
+
+        public List<Livro> ListarTodos()
+        {
+            return Livros;
+        }
+
+        public void Remover(int id)
+        {
+            Livro? livroParaExclusao = BuscarPorId(id);
+            
+            if (!(livroParaExclusao is null))
+            {
+                Livros.Remove(livroParaExclusao);
+            }
+            else
+            {
+                throw new Exception("LIVRO NÃO ENCONTRADO.");
+            }
+        }
     }
 }
