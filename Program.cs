5da79adb2c6b8c9bb85611a1c67b9e05096d3ed5
Victor-Finotto <victor.finotto@outlook.com>
2025-04-08 22:38:13 -0300

    fix: adiciona Program.cs não versionado anteriormente

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using BibliotecaConsoleApp.Entidades;
+using BibliotecaConsoleApp.Menus;
+using BibliotecaConsoleApp.Repositorios;
+using BibliotecaConsoleApp.Servicos;
+
+LivroRepositorio livroRepositorio = new();
+LivroServico livroServico = new(livroRepositorio);
+MenuBiblioteca menuBiblioteca = new(livroServico);
+
+menuBiblioteca.Executar();
+
